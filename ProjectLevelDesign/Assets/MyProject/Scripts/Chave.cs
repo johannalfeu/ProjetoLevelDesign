d90f076798ee5895de53cc7abc95353c8b6096e0
johannalfeu <johannalfeu@gmail.com>
2021-11-14 23:12:59 -0300

    Upd
    Porta abre com chave

---
+++ ProjectLevelDesign/Assets/MyProject/Scripts/Chave.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chave : MonoBehaviour
+{
+    public float distanciaDaChave = 3;
+    public bool temChave;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temChave = false;
+        player = GameObject.FindWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < distanciaDaChave)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && temChave == false)
+            {
+                temChave = true;
+                Destroy(gameObject);
+            }
+        }
+
+
+    }
+
+
+}
--- ProjectLevelDesign/Assets/MyProject/Scripts/Porta.cs
+++ ProjectLevelDesign/Assets/MyProject/Scripts/Porta.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 
 public class Porta : MonoBehaviour
 {
+    public GameObject key;
+    public bool ContemChave;
+
     public Animator anim;
 
     private bool colisao;
     private bool PortaAberta = false;
     void Start()
     {
-        
+        ContemChave = false;
     }
 
-    
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && colisao)
+        if (Input.GetKeyDown(KeyCode.E) && colisao && ContemChave == true)
         {
             PortaAberta = true;
             anim.SetTrigger("Abrir");
         }
+
+        ContemChave = key.GetComponent<Chave>().temChave;
     }
 
-     void OnTriggerEnter(Collider col) {
-        if(col.gameObject.CompareTag("Player"))
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
         {
             colisao = true;
         }
     }
 
-     void OnTriggerExit(Collider col) {
-        if(col.gameObject.CompareTag("Player"))
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
         {
-            if(PortaAberta)
+            if (PortaAberta)
             {
                 anim.SetTrigger("Fechar");
             }
